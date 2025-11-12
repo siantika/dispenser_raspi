fa75b225526542d80c4023daaf1e0a17de9ca3cc
sian <csiantika@gmail.com>
2025-11-12 22:56:47 +0800

    Implements logger

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
+#!/usr/bin/env python3
+import multiprocessing as mp
+from utils.logger import setup_logger, get_queue, listener_process
 
-def app():
-    pass 
+def worker(name):
+    log = setup_logger(name, get_queue())
+    log.info(f"Hello from {name}!")
 
 if __name__ == '__main__':
-    app()
+    # Start listener
+    listener = mp.Process(target=listener_process, args=(get_queue(),), daemon=True)
+    listener.start()
+
+    # Setup main logger
+    log = setup_logger("MAIN")
+    log.info("Main process started")
+
+    # Spawn workers
+    p1 = mp.Process(target=worker, args=("Worker-1",))
+    p2 = mp.Process(target=worker, args=("Worker-2",))
+    p1.start(); p2.start()
+    p1.join(); p2.join()
+
+    # Stop listener
+    get_queue().put(None)
+    listener.join()
---
+++ utils/logger.py
@@ -0,0 +1,51 @@
+# utils/logger.py
+import logging
+import logging.handlers
+import multiprocessing as mp
+from typing import Optional
+
+# Queue global untuk logging antar proses
+_log_queue: Optional[mp.Queue] = None
+
+def get_queue() -> mp.Queue:
+    global _log_queue
+    if _log_queue is None:
+        _log_queue = mp.Queue(-1)  # unlimited size
+    return _log_queue
+
+def listener_configurer():
+    root = logging.getLogger()
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        '[%(asctime)s] %(levelname)-8s [%(processName)s] %(message)s',
+        datefmt='%H:%M:%S'
+    )
+    handler.setFormatter(formatter)
+    root.addHandler(handler)
+    root.setLevel(logging.INFO)
+
+def listener_process(queue: mp.Queue):
+    listener_configurer()
+    while True:
+        try:
+            record = queue.get()
+            if record is None:  # sinyal shutdown
+                break
+            logger = logging.getLogger(record.name)
+            logger.handle(record)
+        except Exception:
+            pass
+
+def worker_configurer(queue: mp.Queue):
+    h = logging.handlers.QueueHandler(queue)
+    root = logging.getLogger()
+    root.addHandler(h)
+    root.setLevel(logging.INFO)
+
+def setup_logger(name: str = "app", queue: Optional[mp.Queue] = None):
+    if queue is None:
+        q = get_queue()
+        worker_configurer(q)
+    else:
+        worker_configurer(queue)
+    return logging.getLogger(name)
