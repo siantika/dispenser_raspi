9b13c2a746063c244fead10a327471fa55a912bd
sian <csiantika@gmail.com>
2025-11-17 22:06:58 +0800

    Implements printer driver

---
+++ src/dispenser_parkir/hardware/printer.py
@@ -0,0 +1,42 @@
+from escpos.printer import Usb
+from typing import Protocol
+
+
+class PrinterDriver(Protocol):
+    def text(self, txt: str) -> None: ...
+    def barcode(
+        self,
+        code: str,
+        bc_type: str,
+        height: int = 64,
+        width: int = 3,
+        pos: str = "BELOW",
+        font: str = "A",
+    ) -> None: ...
+    def cut(self) -> None: ...
+    def close(self) -> None: ...
+
+
+class UsbEscposDriver(PrinterDriver):
+    def __init__(self, vid: int, pid: int, timeout: float = 1.0):
+        self._p = Usb(vid, pid, timeout=timeout)
+
+    def text(self, txt: str) -> None:
+        self._p.text(txt)
+
+    def barcode(
+        self,
+        code: str,
+        bc_type: str,
+        height: int = 64,
+        width: int = 3,
+        pos: str = "BELOW",
+        font: str = "A",
+    ) -> None:
+        self._p.barcode(code, bc_type, height, width, pos, font)
+
+    def cut(self) -> None:
+        self._p.cut()
+
+    def close(self) -> None:
+        self._p.close()
