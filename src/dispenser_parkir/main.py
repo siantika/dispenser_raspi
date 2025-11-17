ac1886e803e7a38d70ff4589bfc38148c1b7d814
sian <csiantika@gmail.com>
2025-11-17 22:15:10 +0800

    test printer

--- src/dispenser_parkir/main.py
+++ src/dispenser_parkir/main.py
@@ -20,28 +20,53 @@ inget isi watchdog
 
 
 #!sourc
+import datetime
 import multiprocessing as mp
 from utils.logger import setup_logger, get_queue, listener_process
+from hardware.printer import UsbEscposDriver as Printer
 
 def worker(name):
     log = setup_logger(name, get_queue())
     log.info(f"Hello from {name}!")
 
 if __name__ == '__main__':
+    
+    printer = Printer()
+    
+    printer.text("==== TEST PRINT ====\n")
+    printer.text(f"Datetime: {datetime.datetime.now()}\n")
+    printer.text("--------------------\n")
+
+    # Print text
+    printer.text("Hello from Python ESC/POS driver!\n")
+    printer.text("Ini adalah test printing.\n\n")
+
+    # Print barcode
+    printer.text("Barcode (CODE128):\n")
+    printer.barcode("123456789012", "CODE128")
+
+    # Cutting paper
+    printer.cut()
+
+    # Close
+    printer.close()
+    print("Berhasil print test ke printer!")
+
     # Start listener
-    listener = mp.Process(target=listener_process, args=(get_queue(),), daemon=True)
-    listener.start()
+    # listener = mp.Process(target=listener_process, args=(get_queue(),), daemon=True)
+    # listener.start()
 
-    # Setup main logger
-    log = setup_logger("MAIN")
-    log.info("Main process started")
+    # # Setup main logger
+    # log = setup_logger("MAIN")
+    # log.info("Main process started")
 
-    # Spawn workers
-    p1 = mp.Process(target=worker, args=("Worker-1",))
-    p2 = mp.Process(target=worker, args=("Worker-2",))
-    p1.start(); p2.start()
-    p1.join(); p2.join()
+    # # Spawn workers
+    # p1 = mp.Process(target=worker, args=("Worker-1",))
+    # p2 = mp.Process(target=worker, args=("Worker-2",))
+    # p1.start(); p2.start()
+    # p1.join(); p2.join()
 
-    # Stop listener
-    get_queue().put(None)
-    listener.join()
+    # # Stop listener
+    # get_queue().put(None)
+    # listener.join()
+    
