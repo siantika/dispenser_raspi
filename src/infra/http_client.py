84f1553acc0016dc75d0795cee9d9de67c4b17cc
sian <csiantika@gmail.com>
2025-12-05 17:17:50 +0800

    Implement to async

--- src/infra/http_client.py
+++ src/infra/http_client.py
@@ -1,24 +1,20 @@
-import requests
+import httpx
 
 
-class HttpClient:
+class AsyncHttpClient:
     def __init__(self, base_url: str, timeout: int = 5):
         self.base_url = base_url
-        self.session = requests.Session()
-        self.timeout = timeout
+        self.session = httpx.AsyncClient(base_url=base_url, timeout=timeout)
 
-    def post(self, path: str, json: dict):
-        url = f"{self.base_url}{path}"
-        resp = self.session.post(url, json=json, timeout=self.timeout)
+    async def get(self, path: str, params: dict = None):
+        resp = await self.session.get(path, params=params)
         resp.raise_for_status()
         return resp
 
-    def get(self, path: str, params: dict = None):
-        url = f"{self.base_url}{path}"
-        resp = self.session.get(url, params=params, timeout=self.timeout)
+    async def post(self, path: str, json: dict):
+        resp = await self.session.post(path, json=json)
         resp.raise_for_status()
         return resp
 
-    def close(self):
-        """Close underlying session when application shut down"""
-        self.session.close()
+    async def close(self):
+        await self.session.aclose()
