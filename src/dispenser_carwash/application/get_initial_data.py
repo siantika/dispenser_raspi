3b15b0c4e8a635809f5a8865145d24c8815076c8
sian <csiantika@gmail.com>
2025-12-06 21:20:51 +0800

    fix get-init-data returns the correct format

--- src/dispenser_carwash/application/get_initial_data.py
+++ src/dispenser_carwash/application/get_initial_data.py
@@ -1,11 +1,25 @@
+from typing import Any, Dict
+
 from dispenser_carwash.domain.interfaces.repositories.i_last_ticket_number import (
     ILastTicketNumberRepository,
+)
+from dispenser_carwash.domain.interfaces.repositories.i_service_type_repo import (
+    IServiceTypeRepository,
 )
 
 
 class GetInitialDataUseCase:
-    def __init__(self, repo:ILastTicketNumberRepository):
-        self.repo = repo 
+    def __init__(self, last_ticket_repo:ILastTicketNumberRepository,
+                 service_type_repo:IServiceTypeRepository):
+        self.last_ticket_repo = last_ticket_repo
+        self.service_type_repo = service_type_repo 
         
-    async def execute(self):
-        return await self.repo.get()
+    async def execute(self)-> Dict[str, Any]:
+        last_ticket = await self.last_ticket_repo.get()
+        list_of_services = await self.service_type_repo.list_all()
+        
+        return {
+            "last_ticket": last_ticket,
+            "list_of_services": list_of_services 
+        }
+        
