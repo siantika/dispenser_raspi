f1461845e77108faf3ba797e2b42c82110a2408b
sian <csiantika@gmail.com>
2025-12-07 21:54:21 +0800

    handle service_name is empty

--- src/dispenser_carwash/application/select_service_uc.py
+++ src/dispenser_carwash/application/select_service_uc.py
@@ -1,7 +1,9 @@
 from typing import List, Optional
 
 from dispenser_carwash.domain.entities.service_type import ServiceType
+from dispenser_carwash.utils.logger import setup_logger
 
+logger = setup_logger("select service - uc")
 
 class SelectServiceUseCase:
     def __init__(self):
@@ -11,6 +13,9 @@ class SelectServiceUseCase:
         self.list_of_services = value
     
     def execute(self, service_name: str) -> Optional[ServiceType]:
+        if service_name is None:
+            logger.info(f" service with name:{service_name} is empty, check the init data")
+            return 
         return next(
             (item for item in self.list_of_services if item.name == service_name),
             None
